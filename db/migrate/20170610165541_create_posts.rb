bce99a6c7904d71fef29a1dbfea422419fa00caa
prasad <prasad@riktamtech.com>
2017-06-12 13:44:36 +0530

    Updated Migrations

--- db/migrate/20170610165541_create_posts.rb
+++ db/migrate/20170610165541_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
-      t.string :content
-      t.string :user
+      t.text :content
+      t.integer :user
 
       t.timestamps
     end
